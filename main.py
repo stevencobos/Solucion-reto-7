0f6c9059e42b25a581425b5c7f5961a8d8ec1330
stevencobos <84248900+stevencobos@users.noreply.github.com>
2021-06-21 20:06:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
+#########################################################
 x = ['Python','Java' ,'PHP', 'JavaScript', 'C#', 'C++']
 popularity = [21.3, 16.1, 8.7, 9, 7.4, 6.5]
+#########################################################
 x_pos = [i for i, _ in enumerate(x)]
 plt.bar(x_pos, popularity, color='green')
+#########################################################
 plt.xlabel('Lenguaje')
 plt.ylabel('Popularidad')
 plt.title('Popularidad del lenguaje de programación\n en todo el mundo')
+#########################################################
 plt.xticks(x_pos, x)
 
 plt.minorticks_on()
